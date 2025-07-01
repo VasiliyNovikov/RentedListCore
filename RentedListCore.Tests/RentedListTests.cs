0d94a1ef81ef0e633e88a11f08315fdf5f0c7027
Vasyl Novikov <vasiliy.novikov@outlook.com>
2025-07-01 14:47:15 +0300

    Merge additional RentedList tests

--- RentedListCore.Tests/RentedListTests.cs
+++ RentedListCore.Tests/RentedListTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RentedListCore.Tests;
 
@@ -81,5 +82,65 @@ public class RentedListTests
         list.Dispose();
         Assert.AreEqual(0, list.Count);
         Assert.AreEqual(0, list.Capacity);
+    }
+
+    [TestMethod]
+    public void AddRange_ShouldAppendItems()
+    {
+        int[] initial = [1, 2];
+        int[] additional = [3, 4];
+        using RentedList<int> list = new(initial);
+        list.AddRange(additional);
+        int[] expected = [1, 2, 3, 4];
+        CollectionAssert.AreEqual(expected, list);
+    }
+
+    [TestMethod]
+    public void Insert_ShouldInsertInMiddle()
+    {
+        using RentedList<int> list = [1, 3];
+        list.Insert(1, 2);
+        int[] expected = [1, 2, 3];
+        CollectionAssert.AreEqual(expected, list);
+    }
+
+    [TestMethod]
+    public void Remove_ShouldRemoveFirstOccurrence()
+    {
+        using RentedList<int> list = [1, 2, 3, 2];
+        bool removed = list.Remove(2);
+        Assert.IsTrue(removed);
+        int[] expected = [1, 3, 2];
+        CollectionAssert.AreEqual(expected, list);
+    }
+
+    [TestMethod]
+    public void Enumeration_ShouldIterateAllItems()
+    {
+        int[] values = [4, 5, 6];
+        using RentedList<int> list = [4, 5, 6];
+        var result = new List<int>();
+        foreach (var v in list)
+            result.Add(v);
+        CollectionAssert.AreEqual(values, result);
+    }
+
+    [TestMethod]
+    public void CopyTo_ShouldCopyItemsToArray()
+    {
+        using RentedList<int> list = [7, 8];
+        int[] target = new int[2];
+        list.CopyTo(target, 0);
+        CollectionAssert.AreEqual(list, target);
+    }
+
+    [TestMethod]
+    public void ContainsAndIndexOf_ShouldReturnCorrectValues()
+    {
+        using RentedList<int> list = [1, 2, 3];
+        Assert.IsTrue(list.Contains(2));
+        Assert.IsFalse(list.Contains(4));
+        Assert.AreEqual(1, list.IndexOf(2));
+        Assert.AreEqual(-1, list.IndexOf(4));
     }
 }

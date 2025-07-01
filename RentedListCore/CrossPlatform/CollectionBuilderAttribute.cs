ac2e18afa6cc29a4355b0edd72106e229e52c885
Vasyl Novikov <vasiliy.novikov@outlook.com>
2025-07-01 15:40:44 +0300

    Add capacity validation and optimize ArraySegment usage (#7)

--- RentedListCore/CrossPlatform/CollectionBuilderAttribute.cs
+++ RentedListCore/CrossPlatform/CollectionBuilderAttribute.cs
@@ -1,5 +1,4 @@
 ﻿#if !NET8_0_OR_GREATER
-#pragma warning disable IDE0055 // False positive
 namespace System.Runtime.CompilerServices;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, Inherited = false)]
